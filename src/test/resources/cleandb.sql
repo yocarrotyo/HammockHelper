07281b5bd6e96c584973422f12878320841d67d6
yocarrotyo <chughes3@madisoncollege.edu>
2020-10-13 20:00:14 -0500

    Indie prj up to date with ex 5 content

--- src/test/resources/cleandb.sql
+++ src/test/resources/cleandb.sql
@@ -1,3 +1,4 @@
+delete from role;
 delete from user;
-INSERT INTO user VALUES (1,'chihiro963','Caroline','Hughes','hughe297@gmail.com','5Mith5'),(2,'blond2016','Frank','Ocean','grammywinner@gmail.com','$0L0$'),(3,'aardvark96','Arthur','Read','iLoveBooks@gmail.com','DWISANNOYING'),(4,'rockhead929','John','McPhee','rockhead929@yahoo.com','ilovegeology'),(5,'iBeezinDaTrap','Nicole','Minaj','rapQueen4lyfe@gmail.com','p!nkpr!nt'),(6,'itsmyworld','Elmo','','elmosworld@sesame.com','123ABC');
-delete from role; INSERT INTO role VALUES (1, 'Camper', 'rockhead929'),(2, 'Camper', 'blond2016'),(3, 'Camper', 'itsmyworld'),(4, 'Parkemployee', 'aardvark96'),(5, 'Parkemployee', 'chihiro963'),(6, 'Camper', 'iBeezinDaTrap');
+INSERT INTO user VALUES (1,'chihiro963','Caroline','Hughes','hughe297@gmail.com','5Mith5'),(2,'blond2016','Frank','Ocean','grammywinner@gmail.com','$0L0$'),(3,'aardvark96','Arthur','Read','iLoveBooks@gmail.com','DWISANNOYING'),(4,'rockhead929','John','McPhee','rockhead929@yahoo.com','ilovegeology'),(5,'iBeezinDaTrap','Nicki','Minaj','rapQueen4lyfe@gmail.com','p!nkpr!nt'),(6,'itsmyworld','Elmo','','elmosworld@sesame.com','123ABC');
+INSERT INTO role VALUES (1,'Camper','rockhead929'),(2, 'Camper', 'blond2016'),(3, 'Camper', 'itsmyworld'),(4, 'Parkemployee', 'aardvark96'),(5, 'Parkemployee', 'chihiro963'),(6, 'Camper', 'iBeezinDaTrap');
